231a8d333dad2da14c948a67cc0a4c9c73a7c59e
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-12-29 17:22:44 -0300

    set AppProvider on app

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'antd/dist/antd.css';
+import { AppProvider } from '../hooks';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+    <Component {...pageProps} />
+    </AppProvider>
+  )
 }
 
 export default MyApp
